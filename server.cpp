be2818aed57b3849090ac65d21aac62e643bfe88
Ubuntu <liyuliang001@gmail.com>
2018-09-17 00:46:53 +0000

    add more config for server

--- server.cpp
+++ server.cpp
@@ -7,11 +7,21 @@
 using namespace std;
 using namespace bright_lib::tcp_socket;
 
+int buf_size = 100000;
+
+void print_usage(void){
+	printf("Usage: ./server [<options>...]\n");
+	printf("Options:\n");
+	printf("        -p      specify the destination port\n");
+	printf("        -b      specify the msg buffer size\n");
+}
+
 class ReceiverHandler: public ServerHandler{
 public:
-#define BUF_SZ 100000lu
-	char buf[BUF_SZ];
+	char *buf;
 	virtual int execute(){
+		int buf_sz = buf_size;
+		buf = new char[buf_sz];
 		#if 0
 		int tmp=100;
 		socklen_t optlen=100;
@@ -26,22 +36,38 @@ public:
 		recv(size);
 		// send 
 		for (uint64_t sent = 0; sent < size;){
-			uint64_t to_send = min(size - sent, BUF_SZ);
+			uint64_t to_send = min(size - sent, (uint64_t)buf_sz);
 			ssize_t ret = send_raw(buf, to_send);
 			sent += ret;
 		}
 		close();
+		delete[] buf;
 		return 0;
 	}
 };
 
 int main(int argc, char** argv){
-	if (argc != 2){
-		printf("Usage: ./server <port>\n");
+	uint16_t port = 0;
+	// parse args
+	for (int opt; (opt = getopt(argc, argv, "p:b:h")) != -1; ){
+		switch (opt){
+			case 'p':
+				port = stoi(optarg);
+				break;
+			case 'b':
+				buf_size = stoi(optarg);
+				break;
+			case 'h':
+			default:
+				print_usage();
+				return 0;
+		}
+	}
+	if (port == 0){
+		print_usage();
 		return 0;
 	}
 
-	uint16_t port = stoi(argv[1]);
 	Server<ReceiverHandler> s;
 	s.listen(port, 1000, false);
 	return 0;
